1b1bf7c1a8fa053fa84ded25a30c6168a7dfb461
charloLito <1362270@cstj.qc.ca>
2017-12-13 13:07:10 -0500

    Réussi le création de compte

--- js/charlesJs/register.js
+++ js/charlesJs/register.js
@@ -1,6 +1,7 @@
 
 
 const REGISTER_URL="https://andromia-olivierissacstj.c9users.io/register/";
+const LOGIN_URL="https://andromia-olivierissacstj.c9users.io/login/";
 $(document).ready(function(){
     
     
@@ -12,12 +13,34 @@ $(document).ready(function(){
         var pseudo=$("#pseudo").val();
         
         var creationCompte = {};
-        creationCompte.courriel=courriel;
-        creationCompte.motDePasse=motDePasse;
+        creationCompte.email=courriel;
+        creationCompte.password=motDePasse;
         creationCompte.pseudo=pseudo;
-        console.log(creationCompte);
-        $.post(REGISTER_URL,creationCompte,function(){
-            alert("done");
-        },"json");
+        console.log(JSON.stringify(creationCompte));
+        
+        $.ajax({
+            url: REGISTER_URL,
+            type: 'POST',
+            data: JSON.stringify(creationCompte),
+            contentType: 'application/json; charset=utf-8',
+            dataType: 'json',
+            async: false,
+            success: function() {
+                var login={};
+                login.username=courriel;
+                login.password=motDePasse;
+                $.ajax({
+                    url: LOGIN_URL,
+                    type: 'POST',
+                    data: JSON.stringify(login),
+                    contentType: 'application/json; charset=utf-8',
+                    dataType: 'json',
+                    async: false,
+                    success: function() {
+                        window.location.href="index.html";
+                    }
+                });
+            }
+        });
     });
 });
