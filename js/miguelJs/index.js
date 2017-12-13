25d1e22998c75ea84a89c576573dc4b894c4b5fa
Miguel Dupuis <1547201@cstj.qc.ca>
2017-12-13 11:22:40 -0500

    fix erreur Accueil

---
+++ js/miguelJs/index.js
@@ -0,0 +1,25 @@
+const RUNES_URL = "https://https://andromia-olivierissacstj.c9users.io/explorateurs/";
+
+
+// :uuidExplorateur/runes"
+
+
+$(document).ready(function() {
+    $.get(SERVICE_URL,function(explorateur){
+        let lstRunes;
+
+        for
+
+
+
+
+        <li class="dropdown nav-item ">
+        <img src="img/runes/air.png">
+    </li>
+    <li class="dropdown nav-item ">
+        <p class="text-orientation-right-css">100</span></p>
+    </li>
+
+
+    })
+})
