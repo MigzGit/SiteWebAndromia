bf1fe4a145cc9929fd075b5c45b48161a0bf99eb
charloLito <1362270@cstj.qc.ca>
2017-12-13 14:29:30 -0500

    Mit le token dans le local storage lors de la connexion et le href du explorateur

--- js/charlesJs/register.js
+++ js/charlesJs/register.js
@@ -16,8 +16,6 @@ $(document).ready(function(){
         creationCompte.email=courriel;
         creationCompte.password=motDePasse;
         creationCompte.pseudo=pseudo;
-        console.log(JSON.stringify(creationCompte));
-        
         $.ajax({
             url: REGISTER_URL,
             type: 'POST',
@@ -26,21 +24,22 @@ $(document).ready(function(){
             dataType: 'json',
             async: false,
             success: function() {
-                var login={};
-                login.username=courriel;
-                login.password=motDePasse;
-                $.ajax({
-                    url: LOGIN_URL,
-                    type: 'POST',
-                    data: JSON.stringify(login),
-                    contentType: 'application/json; charset=utf-8',
-                    dataType: 'json',
-                    async: false,
-                    success: function() {
-                        window.location.href="index.html";
-                    }
-                });
             }
-        });
+        });  
+        window.location="login.html"; 
+        /*
+        creationCompte=JSON.stringify(creationCompte);
+        console.log(creationCompte);
+        $.post(REGISTER_URL,creationCompte,function(){
+            console.log(creationCompte);
+            
+        },"json")
+        */
+        /*
+        
+                
+           
+        */
+        
     });
 });
