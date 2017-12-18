b30a953ca16cc78720caaa30a7ffc980c155937e
Charles BELISLE <1362270@cstj.qc.ca>
2017-12-18 12:12:24 -0500

    Enlever les fichiers qui servent a rien et ajouter une regle qui s'assure qu'un courriel à un @

--- js/charlesJs/register.js
+++ js/charlesJs/register.js
@@ -13,37 +13,49 @@ $(document).ready(function(){
         var courriel=$(".courriel").val();
         var pseudo=$("#pseudo").val();
         
+        
+        
         var creationCompte = {};
         creationCompte.email=courriel;
         creationCompte.password=motDePasse;
         creationCompte.pseudo=pseudo;
-        $.ajax({
-            url: REGISTER_URL,
-            type: 'POST',
-            data: JSON.stringify(creationCompte),
-            contentType: 'application/json; charset=utf-8',
-            dataType: 'json',
-            async: true,
-            statusCode:{
-                201:function() {
-                    window.location="login.html"; 
-                    
+        
+        if(courriel.indexOf("@")>=0)
+        {
+        
+            $.ajax({
+                url: REGISTER_URL,
+                type: 'POST',
+                data: JSON.stringify(creationCompte),
+                contentType: 'application/json; charset=utf-8',
+                dataType: 'json',
+                async: true,
+                statusCode:{
+                    201:function() {
+                        window.location="login.html"; 
+                        
+                    },
+                    415:function(){
+                        $("#messageErreur").append("<p>Courriel vide.</p>")
+                        setTimeout(function(){
+                            $("p").remove();
+                        },5000); 
+                    }
                 },
-                415:function(){
-                    $("#messageErreur").append("<p>Courriel vide.</p>")
+                error:function(a){
+                    $("#messageErreur").append("<p>Courriel ou mot de passe invalide.</p>")
                     setTimeout(function(){
                         $("p").remove();
                     },5000); 
                 }
-            },
-            error:function(a){
-                console.log(a);
-                $("#messageErreur").append("<p>Courriel ou mot de passe invalide.</p>")
-                setTimeout(function(){
-                      $("p").remove();
-                },5000); 
-            }
-        });  
+            });  
+        }
+        else{
+            $("#messageErreur").append("<p>Courriel invalide.</p>")
+            setTimeout(function(){
+                $("p").remove();
+            },5000); 
+        }
         /*
         creationCompte=JSON.stringify(creationCompte);
         console.log(creationCompte);
